2891e7d9825a55430d6f5cf0e0af4fb11bbeaae2
Liudmila <frolova47@inbox.ru>
2023-03-05 18:18:46 +0400

    решена задача 1

--- task01/Program.cs
+++ task01/Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.Write("введите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int max = 0;
+if (n1>n2)
+    Console.WriteLine($"максимальное число {max=n1}");
+else if (n2>n1)
+    Console.WriteLine($"максимальное число {max=n2}");
+else Console.Write("числа равны");
