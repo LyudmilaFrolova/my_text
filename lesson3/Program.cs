a844398ac80d66f3be2efc4a769af2357b0840fc
Liudmila <frolova47@inbox.ru>
2023-03-11 16:39:56 +0400

    решены задачи в ходе семинара (с помощью преподавателя)))

---
+++ lesson3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите число 1:");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число 2:");
+int n2 = Convert.ToInt32(Console.ReadLine());
+if (n1*n1 == n2 || n2*n2 == n1)
+    Console.WriteLine("да");
+else 
+    Console.WriteLine("нет");
