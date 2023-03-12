4bb6500f08557d72610dda6c99b1b9fe2fe2085b
LyudmilaFrolova <126704981+LyudmilaFrolova@users.noreply.github.com>
2023-03-12 18:33:56 +0400

    Delete lesson 6 directory

--- lesson 6/Program.cs
+++
@@ -1,8 +0,0 @@
-﻿// напишите программу, которая принимает на вход число N и выводит квадраты от 1 до N
-Console.WriteLine("введите число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("квадраты от 1 до введенного числа: ");
-for (int i = 1; i <= n; i++) 
-     Console.Write($"{Math.Pow(i,2)} ");
-    
-
