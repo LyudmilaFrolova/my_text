253fb547f372cc1896677c42588bf78cad11183a
Liudmila <frolova47@inbox.ru>
2023-03-12 17:00:16 +0400

    добавлены файлы с решением задач к семинару 2: seminar 2 task 10 seminar 2 task 13 seminar 2 task 15

---
+++ lesson 6/Program.cs
@@ -0,0 +1,8 @@
+﻿// напишите программу, которая принимает на вход число N и выводит квадраты от 1 до N
+Console.WriteLine("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("квадраты от 1 до введенного числа: ");
+for (int i = 1; i <= n; i++) 
+     Console.Write($"{Math.Pow(i,2)} ");
+    
+
---
+++ seminar 2 task 13/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Write("Введите любое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string numStr = Convert.ToString(n);
+int numIndex = numStr.Length;
+if (numIndex > 2) Console.WriteLine($"Третья цифра введённого числа: {numStr[2]}");
+else Console.WriteLine("третьей цифры нет");
